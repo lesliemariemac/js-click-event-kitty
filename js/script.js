144ea0d72cb4a3306bfe91cb79889d2a2ba5b014
Leslie M <lesliemariemac@gmail.com>
2023-11-19 09:39:45 -0500

    Added if else statements

--- js/script.js
+++ js/script.js
@@ -5,6 +5,19 @@ var cat = document.querySelector(".cat");
 console.log(cat);
 
 button.addEventListener("click", function () {
-    button.innerText = "It's working";
-    cat.classList = ".show-cat";
+    //button.innerText = "It's working";
+    //cat.classList = ".show-cat";
+    if (cat.classList.contains("show")) {
+    //console.log("yes"); 
+    cat.classList.remove("show");
+    button.innerText = "Wait, come back!";
+    button.classList.add("disappear");
+    } else {
+      cat.classList.add("show"); 
+      //console.log ("no");
+      button.innerText = "Shoo, cat!!";
+      button.classList.remove("disappear");
+      
+      
+}
 });
